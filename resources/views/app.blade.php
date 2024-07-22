d053cc6b8b353375c951cd29cd607781524da9f5
orekode <shalommckdavid@gmail.com>
2024-07-22 12:05:47 +0000

    fixed error from reactification of home page

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,9 +7,9 @@
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        <link rel="stylesheet" href="/styles/home.css">
+        <!-- <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" /> -->
+        
 
         <!-- Scripts -->
         @routes
